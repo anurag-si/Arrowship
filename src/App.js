71d9422b2a7cf6d28c13653087c7489d2fe5a442
Anurag Singh <76003552+anurag-si@users.noreply.github.com>
2022-10-11 19:08:10 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Name from "./Components/Name/Name";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Arrowship</header>
-    </div>
-  );
+  return <div className="App">
+    <Name />
+  </div>;
 }
 
 export default App;
---
+++ src/Components/Name/Name.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Name.css'
+
+const Name = () => {
+  return (
+    <div className='name'>
+        <div className='name1'>
+        Arrowship
+        </div>
+       </div>
+  )
+}
+
+export default Name
